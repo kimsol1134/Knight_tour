4750bb4c39af809556e728f9e10552ed1061462f
sol kim <kimsol1134@naver.com>
2025-05-12 10:52:18 +0900

    app

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'knight_tour_v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  './open_graph.png'
+  // 필요 시 JS/CSS 파일 추가
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((resp) => resp || fetch(e.request))
+  );
+});
